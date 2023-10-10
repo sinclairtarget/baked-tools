20527071988b2c946073c2c0e5fddb13fed1187c
camdotcom14 <camerontarget@gmail.com>
2023-10-10 04:59:37 -0400

    Delivery Sync Finalized
    In this commit, I got rid of some of the side scripts that I used to help me write delivery sync. Delivery sync now syncs contacts, sg data (notes and versions) and sorts sg data by sg_shot_code using python's 'sorted'

--- baked_delivery_sync/delivery_sync.py
+++ baked_delivery_sync/delivery_sync.py
@@ -1,87 +1,150 @@
+from flask import escape, request
 import shotgun_api3
 import gspread
 from datetime import datetime
+import os
+import json
 
-##############
-#GET SG STUFF#
-##############
+def sync_shotgrid_to_sheet(request):
+    project_name = request.args.get('project_name')
+    delivery_iteration = request.args.get('delivery_iteration')
 
-# Retrieve API KEY
-with open('baked_delivery_sync/SG-API-KEY.txt', 'r') as f:
-    retrieved_key = f.read().strip()
+    if not project_name:
+        return 'Project name is missing', 400
 
-# Define ShotGrid server and script credentials
-SHOTGUN_URL = 'https://baked.shotgunstudio.com'
-SCRIPT_NAME = 'Tidbyt'
-SCRIPT_KEY = retrieved_key
+    ##############
+    # GET SG STUFF#
+    ##############
 
-# Connect to ShotGrid
-sg = shotgun_api3.Shotgun(SHOTGUN_URL, SCRIPT_NAME, SCRIPT_KEY)
+    # Retrieve API KEY from environment variable
+    retrieved_key = os.environ.get('SG_API_KEY')
+    if not retrieved_key:
+        return 'ShotGrid API key is missing from environment variables', 500
 
-#temporary values - need to pull project name from first 3 characters in cell A1, and deilvery_iteration from last 2 (if applicable)- 
-#-in worksheet = Submission --- can be delivered in http?
-project_name = "BRC"
-delivery_iteration = "02"
+    # Define ShotGrid server and script credentials
+    SHOTGUN_URL = 'https://baked.shotgunstudio.com'
+    SCRIPT_NAME = 'Tidbyt'
+    SCRIPT_KEY = retrieved_key
 
-# Define version filters to narrow down the query
-filters = [
-    ["project.Project.name", "is", project_name], ["sg_status_list", "is", "note"]
-]
+    # Connect to ShotGrid
+    sg = shotgun_api3.Shotgun(SHOTGUN_URL, SCRIPT_NAME, SCRIPT_KEY)
 
-# Define note filters to narrow down the query
-note_filters = [
-    ["project.Project.name", "is", project_name], ["content", "contains", "FROM PRODUCTION"]
-]
+    # Define version filters
+    filters = [
+        ["project.Project.name", "is", project_name], ["sg_status_list", "is", "cli"]
+    ]
 
-# Define fields to be retrieved
-fields = ['sg_shot_code', 'code', 'sg_work_description']
+    # Define note filters
+    note_filters = [
+        ["project.Project.name", "is", project_name], ["content", "contains", "FROM PRODUCTION"]
+    ]
 
-# Define note fields to be retrieved
-note_fields = ['content']
+    # Define fields to be retrieved
+    fields = ['sg_shot_code', 'code', 'sg_work_description']
 
-# Query the ShotGrid site
-versions = sg.find("Version", filters, fields)
+    # Define note fields to be retrieved
+    note_fields = ['content', 'addressings_to']
 
-# Query the ShotGrid site
-notes = sg.find("Note", note_filters, note_fields)
+    # Query ShotGrid for versions
+    versions = sg.find("Version", filters, fields)
 
-###############
-#PUT ON GSHEET#
-###############
+    # Sort versions by 'sg_shot_code' in alphabetical order
+    sorted_versions = sorted(versions, key=lambda x: x['sg_shot_code'])
 
-# Format the data from SG as a list of lists (each sub-list is a row)
-formatted_data = [[version[field] for field in fields] for version in versions]
+    # Query ShotGrid for notes
+    notes = sg.find("Note", note_filters, note_fields)
 
-# Format the notes data from SG as a list of lists (each sub-list is a row)
-formatted_notes_data = [[note[field] for field in note_fields] for note in notes]
+    ###############
+    # PUT ON GSHEET#
+    ###############
 
-# Get the current date and time
-now = datetime.now()
+    # Create a mapping between versions and notes
+    version_note_map = {}
+    for version in versions:
+        version_code = version['code']
+        version_note_map[version_code] = []
 
-# Format the datetime object as YYMMDD
-formatted_date = now.strftime("%y%m%d")
+    for note in notes:
+        note_content = note['content']
+        for version_code in version_note_map.keys():
+            if version_code in note_content:
+                version_note_map[version_code].append(note_content)
 
-# remove "_" from below if delivery_iteration = ""
-if delivery_iteration:
-    delivery_iteration_end_string = "_" + delivery_iteration
-else:
-    delivery_iteration_end_string = delivery_iteration
+    # Create a list that includes both versions and corresponding notes
+    formatted_data_with_notes = []
+    for version in sorted_versions:
+        version_code = version['code']
+        version_row = [version[field] for field in fields]
+        notes_for_version = version_note_map.get(version_code, [])
+        version_row.extend(notes_for_version)
+        formatted_data_with_notes.append(version_row)
 
-# user service account to open sheet
-sa = gspread.service_account(filename="baked_delivery_sync/GOOGLE-SHEET-KEYS.json")
-sh = sa.open(project_name + "_BKD_VFX_Submission_" + formatted_date + delivery_iteration_end_string)
+    # Get the current date and time
+    now = datetime.now()
 
-# access correct worksheet
-wks = sh.worksheet("Submission")
+    # Format the datetime object
+    formatted_date = now.strftime("%y%m%d")
 
-# Clear the existing data from the worksheet
-# Fetch the range of cells
-cell_range_to_clear = 'A3:D30'
+    # Update delivery_iteration_end_string based on delivery_iteration
+    if delivery_iteration:
+        delivery_iteration_end_string = "_" + delivery_iteration
+    else:
+        delivery_iteration_end_string = delivery_iteration
 
-# Clear the values
-for cell in cell_range_to_clear:
+    # Retrieve Google Sheet keys from environment variable
+    google_sheet_keys_json = os.environ.get('GOOGLE_SHEET_KEYS')
+    if not google_sheet_keys_json:
+        return 'Google Sheet keys are missing from environment variables', 500
+
+    google_sheet_keys = json.loads(google_sheet_keys_json)
+
+    # Use service account to open sheet
+    sa = gspread.service_account_from_dict(google_sheet_keys)
+
+    ################################
+    ## CONTACTS PULL FROM GSHEET ###
+    ################################
+
+    # Open contacts sheet
+    sh = sa.open(project_name + "_CLIENT_CONTACT_SHEET")
+
+    # access correct contacts worksheet
+    wks = sh.worksheet("Sheet1")
+
+    # get cotacts data
+    contacts_in_data = wks.get("A2:F15")
+
+     # sync contacts
+    contacts_out_data = contacts_in_data
+
+    #open Submission Sheet
+    sh = sa.open(project_name + "_BKD_VFX_Submission_" + formatted_date + delivery_iteration_end_string)
+
+    # Access correct worksheet
+    wks = sh.worksheet("Submission")
+
+    # Clear existing data
+    cell_range_to_clear = 'A3:D30'
     wks.update(cell_range_to_clear, [["" for _ in range(4)] for _ in range(28)])
 
-# update worksheet
-wks.update('A3', formatted_data)
-wks.update('D3', formatted_notes_data)
+    # Update worksheet with both versions and corresponding notes
+    wks.update('A3', formatted_data_with_notes)
+
+    ##############################
+    ## CONTACTS PUSH TO GSHEET ###
+    ##############################
+
+    # access correct contacts worksheet in target sheet
+    wks = sh.worksheet("Contacts")
+
+    # Fetch the range of contacts cells
+    contacts_cell_range_to_clear = 'A3:F15'
+
+    # Clear the values for contacts
+    for cell in contacts_cell_range_to_clear:
+        wks.update(contacts_cell_range_to_clear, [["" for _ in range(6)] for _ in range(13)])
+
+    # update contacts worksheet
+    wks.update('A3', contacts_out_data)
+
+    return 'Sync complete', 200
